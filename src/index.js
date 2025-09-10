93751762b1adfe0f4c8515ada4e53c6e0f4e08a3
beckslange <beckslange96@gmail.com>
2025-09-10 15:16:21 -0600

    created javascript file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.value);
+}
+let searchFormElement = document.querySelector("#search-form");
+// console.log(searchFormElement); -- Do this to make sure that your elements
+//are working correctly. Check the console on the website you are making
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+//the function above is added after the event listener is defined
